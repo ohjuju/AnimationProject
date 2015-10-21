66ac539de364600710724c7dfa541ee4ceec4b7f
OkarOu <ouq68@qq.com>
2015-10-22 00:19:18 +0800

    update animation

--- AnimationPorject/GenViewController.m
+++ AnimationPorject/GenViewController.m
@@ -17,13 +17,18 @@
 @property (nonatomic, strong) UILabel *testLabel;
 @property (nonatomic, strong) UILabel *testLabel1;
 @property (nonatomic, strong) UILabel *testLabel2;
+@property (nonatomic, strong) UILabel *testLabel3;
+@property (nonatomic, strong) UILabel *testLabel4;
 
 @property (nonatomic, strong) CAGradientLayer *gradient;
 @property (nonatomic, strong) UIView *gradientView;
 
 @property (nonatomic, strong) UIView *animationView;
+@property (nonatomic, strong) UIView *stopView;
 
 @property (nonatomic, strong) UIPanGestureRecognizer *recognizer;
+
+@property (nonatomic, assign) CGFloat gestureMinimumTranslation;
 
 @end
 
@@ -34,10 +39,13 @@
 @synthesize testLabel;
 @synthesize testLabel1;
 @synthesize testLabel2;
+@synthesize testLabel3;
+@synthesize testLabel4;
 
 @synthesize gradient;
 @synthesize gradientView;
 @synthesize animationView;
+@synthesize stopView;
 
 @synthesize recognizer;
 
@@ -51,6 +59,9 @@
     
     _testView = [UIView new];
     _testView.backgroundColor = [UIColor whiteColor];
+    _testView.alpha = 1.0f;
+    
+    self.gestureMinimumTranslation = 20.0;
     
     //btn
     startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
@@ -83,9 +94,36 @@
         make.size.mas_equalTo(CGSizeMake(self.view.bounds.size.width, 70));
     }];
     
+    [self initStartView];
+    [self initStopView];
+    
+    _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    
+    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 1)];
+    lineView.backgroundColor = [UIColor grayColor];
+    _tableView.tableHeaderView = lineView;
+    
+    [self.view addSubview:_tableView];
+    [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(animationView.mas_bottom);
+        make.left.and.right.and.bottom.equalTo(self.view);
+    }];
+    
+    recognizer = [UIPanGestureRecognizer new];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)initStartView {
     animationView = [UIView new];
     animationView.backgroundColor = [UIColor whiteColor];
-
+    
     UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 1)];
     topView.backgroundColor = [UIColor grayColor];
     [animationView addSubview:topView];
@@ -133,41 +171,84 @@
         make.top.equalTo(_testView.mas_bottom);
         make.size.mas_equalTo(CGSizeMake(self.view.bounds.size.width, 0));
     }];
-    
-    _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
-    _tableView.delegate = self;
-    _tableView.dataSource = self;
-    
-    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 1)];
-    lineView.backgroundColor = [UIColor grayColor];
-    _tableView.tableHeaderView = lineView;
-    
-    [self.view addSubview:_tableView];
-    [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(animationView.mas_bottom);
-        make.left.and.right.and.bottom.equalTo(self.view);
-    }];
-    
-    recognizer = [UIPanGestureRecognizer new];
-
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)initStopView {
+    stopView = [UIView new];
+    stopView.backgroundColor = [UIColor whiteColor];
+    
+    UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 1)];
+    topView.backgroundColor = [UIColor grayColor];
+    [stopView addSubview:topView];
+    
+    testLabel3 = [UILabel new];
+    testLabel3.backgroundColor = [UIColor whiteColor];
+    testLabel3.text = @"testttttttttttttttttttt11";
+    testLabel3.textAlignment = NSTextAlignmentCenter;
+    testLabel3.hidden = YES;
+    [stopView addSubview:testLabel3];
+    [testLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(animationView);
+        make.bottom.equalTo(animationView);
+        make.size.mas_equalTo(CGSizeMake(self.view.bounds.size.width, 50));
+    }];
+    testLabel4 = [UILabel new];
+    testLabel4.backgroundColor = [UIColor whiteColor];
+    testLabel4.text = @"testttttttttttttttttttt11";
+    testLabel4.textAlignment = NSTextAlignmentCenter;
+    testLabel4.hidden = YES;
+    [stopView addSubview:testLabel4];
+    [testLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(animationView);
+        make.bottom.equalTo(animationView).offset(-50);
+        make.size.mas_equalTo(CGSizeMake(self.view.bounds.size.width, 50));
+    }];
+    
+    testLabel3.alpha = 0.0f;
+    testLabel4.alpha = 0.0f;
+    
+    [self.view addSubview:stopView];
+    [stopView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_testView.mas_bottom);
+        make.size.mas_equalTo(CGSizeMake(self.view.bounds.size.width, 0));
+    }];
 }
 
 #pragma mark - gesture
 - (void)handlePanRecognizer:(UIPanGestureRecognizer *)pan {
     CGPoint translation = [pan translationInView:_tableView];
 //    NSLog(@"%f,%f",translation.x,translation.y);
-    CGFloat animationHeight = animationView.bounds.size.height - fabs(translation.y);
-    NSLog(@"animation height is : %f",animationHeight);
-    if (translation.y < 0 && animationHeight > 0) {
-        [animationView mas_updateConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(_testView.mas_bottom);
-            make.height.mas_equalTo(animationHeight);
-        }];
+//    CGFloat animationHeight = animationView.bounds.size.height - fabs(translation.y);
+//    NSLog(@"animation height is : %f",animationHeight);
+//    if (translation.y < 0 && animationHeight > 0) {
+//        [animationView mas_updateConstraints:^(MASConstraintMaker *make) {
+//            make.top.equalTo(_testView.mas_bottom);
+//            make.height.mas_equalTo(animationHeight);
+//        }];
+//    }
+    if (fabs(translation.y) > self.gestureMinimumTranslation)
+    {
+        BOOL gestureVertical = NO;
+        if (translation.x ==0.0) {
+            gestureVertical = YES;
+        } else {
+            gestureVertical = (fabs(translation.y / translation.x) >5.0);
+        }
+        if (gestureVertical)
+        {
+            if (translation.y >0.0) {
+                //down
+            }else {
+                //up
+                CGFloat animationViewHeight = animationView.bounds.size.height - fabs(translation.y);
+                CGFloat currentAnimationViewHeight = (animationViewHeight<0)?0:animationViewHeight;
+                NSLog(@"currentAnimationViewHeight is :%f",currentAnimationViewHeight);
+                [animationView mas_updateConstraints:^(MASConstraintMaker *make) {
+                    make.top.equalTo(_testView.mas_bottom);
+                    make.height.mas_equalTo(currentAnimationViewHeight);
+                }];
+            }
+        }
     }
 }
 
@@ -243,15 +324,16 @@
 #pragma mark - blur
 - (void) insertTransparentGradient {
     gradientView = [[UIView alloc] init];
-    gradient = [CAGradientLayer layer];
-    CGFloat screenWidth  =[[UIScreen mainScreen] bounds].size.width;
-    gradient.frame = CGRectMake(0, 0, screenWidth,50);
-    gradient.colors = [NSArray arrayWithObjects:
-                       (id)[[UIColor colorWithRed:255/255 green:255/255 blue:255/255  alpha:1.0] CGColor],
-                       (id)[[UIColor colorWithRed:255/255 green:255/255 blue:255/255  alpha:0.3] CGColor], nil];
-    gradient.startPoint = CGPointMake(0.5, 0.0); // default; bottom of the view
-    gradient.endPoint = CGPointMake(0.5, 1.0);   // default; top of the view
-    [gradientView.layer insertSublayer:gradient atIndex:0];
+    gradientView.backgroundColor = [UIColor whiteColor];
+//    gradient = [CAGradientLayer layer];
+//    CGFloat screenWidth  =[[UIScreen mainScreen] bounds].size.width;
+//    gradient.frame = CGRectMake(0, 0, screenWidth,50);
+//    gradient.colors = [NSArray arrayWithObjects:
+//                       (id)[[UIColor colorWithRed:255/255 green:255/255 blue:255/255  alpha:1.0] CGColor],
+//                       (id)[[UIColor colorWithRed:255/255 green:255/255 blue:255/255  alpha:0.3] CGColor], nil];
+//    gradient.startPoint = CGPointMake(0.5, 0.0); // default; bottom of the view
+//    gradient.endPoint = CGPointMake(0.5, 1.0);   // default; top of the view
+//    [gradientView.layer insertSublayer:gradient atIndex:0];
     [self.view addSubview:gradientView];
     [self.view bringSubviewToFront:gradientView];
     [gradientView mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -263,7 +345,7 @@
 - (void)start:(UIButton *)btn {
     [UIView animateWithDuration:0.2f animations:^{
         [animationView mas_updateConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(self.view).offset(_testView.bounds.size.height);
+            make.top.equalTo(_testView.mas_bottom);
             make.height.mas_equalTo(150);
         }];
         startBtn.backgroundColor = [UIColor clearColor];
@@ -293,8 +375,7 @@
 }
 
 - (void)stop:(UIButton *)btn {
-//    [self insertTransparentGradient];
-    [UIView animateWithDuration:0.2f animations:^{
+    [UIView animateWithDuration:1.0f animations:^{
         [animationView mas_updateConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(self.view).offset(_testView.bounds.size.height);
             make.size.mas_equalTo(CGSizeMake(self.view.bounds.size.width, 0));
@@ -305,8 +386,6 @@
         testLabel.alpha = 0.0f;
         testLabel1.alpha = 0.0f;
         testLabel2.alpha = 0.0f;
-
-        
         gradientView.alpha = 0.0f;
         
         [_testView layoutIfNeeded];
