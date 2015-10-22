9daf9ff4ff95d9a353049e0124709e84927e6122
OKAR OU (EUCD-EUC-ISD-OOCLL/ZHA) <okar.ou@oocl.com>
2015-10-22 16:57:48 +0800

    update pan gestureRecognizer operation

--- AnimationPorject/SearchViewController.m
+++ AnimationPorject/SearchViewController.m
@@ -16,6 +16,7 @@
 @property (nonatomic, strong) UIView *lineDynamicView;
 @property (nonatomic, strong) UIView *vesselDynamicView;
 
+@property (nonatomic, strong) UIPanGestureRecognizer *recognizer;
 @end
 
 @implementation SearchViewController
@@ -25,6 +26,8 @@
 
 @synthesize lineDynamicView;
 @synthesize vesselDynamicView;
+
+@synthesize recognizer;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -47,6 +50,7 @@
     _topView = [UIView new];
     _topView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:_topView];
+    [self.view bringSubviewToFront:_topView];
     [_topView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.left.right.equalTo(self.view);
         make.height.mas_equalTo(70);
@@ -81,6 +85,7 @@
 
 - (void)initDynamicViewByDefault {
     _dynamicView = [UIView new];
+    _dynamicView.alpha = 1.0f;
     [self.view addSubview:_dynamicView];
     [_dynamicView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(_topView.mas_bottom);
@@ -90,7 +95,7 @@
     [self initLineDynamicView];
     [self initVesselDynamicView];
     UIView *bottomView = [UIView new];
-    bottomView.backgroundColor = [UIColor blackColor];
+    bottomView.backgroundColor = [UIColor lightGrayColor];
     [_dynamicView addSubview:bottomView];
     [bottomView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.height.mas_equalTo(1);
@@ -103,6 +108,7 @@
 
 - (void)initLineDynamicView {
     lineDynamicView = [UIView new];
+    lineDynamicView.alpha = 1.0f;
     [_dynamicView addSubview:lineDynamicView];
     [lineDynamicView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.left.right.equalTo(_dynamicView);
@@ -132,6 +138,7 @@
 
 - (void)initVesselDynamicView {
     vesselDynamicView = [UIView new];
+    vesselDynamicView.alpha = 1.0f;
     [_dynamicView addSubview:vesselDynamicView];
     [vesselDynamicView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.left.right.equalTo(_dynamicView);
@@ -170,6 +177,81 @@
     }];
 }
 
+#pragma mark - Gesture 
+- (void)initGestureRecognizer {
+    //addgensure
+    recognizer = [UIPanGestureRecognizer new];
+    [_tableView addGestureRecognizer:recognizer];
+    [recognizer addTarget:self action:@selector(tableViewDidPan:)];
+}
+
+- (void)removeGestureRecognizer {
+    [_tableView removeGestureRecognizer:recognizer];
+}
+
+- (void)tableViewDidPan:(UIPanGestureRecognizer *)rec {
+    
+    CGFloat const gestureMinimumTranslation = 10.0f;
+    
+    CGPoint translation = [rec translationInView:_tableView];
+    if (fabs(translation.y) > gestureMinimumTranslation)
+    {
+//        BOOL gestureVertical = NO;
+//        if (translation.x ==0.0) {
+//            gestureVertical = YES;
+//        } else {
+//            gestureVertical = (fabs(translation.y / translation.x) >5.0);
+//        }
+//        if (gestureVertical)
+//        {
+            if (translation.y >0.0) {
+                //down
+            }else {
+                //up
+//                CGFloat animationViewHeight = animationView.bounds.size.height - fabs(translation.y);
+//                CGFloat currentAnimationViewHeight = (animationViewHeight<0)?0:animationViewHeight;
+//                NSLog(@"currentAnimationViewHeight is :%f",currentAnimationViewHeight);
+//                [animationView mas_updateConstraints:^(MASConstraintMaker *make) {
+//                    make.top.equalTo(_testView.mas_bottom);
+//                    make.height.mas_equalTo(currentAnimationViewHeight);
+//                }];
+                
+//                CGFloat dynamicViewOrginY = _dynamicView.bounds.origin.y - fabs(translation.y);
+                
+                NSLog(@"orgin y is : %f",fabs(translation.y));
+                //if user upward > gestureMinimumTranslation,_dynamicView will hidden.
+                if (fabs(translation.y) > gestureMinimumTranslation && rec.state == UIGestureRecognizerStateEnded) {
+                    [UIView animateWithDuration:0.2f animations:^{
+                        [self decreseDynamicViewHeight];
+                        _dynamicView.alpha = 0.0f;
+                        lineDynamicView.alpha = 0.0f;
+                        vesselDynamicView.alpha = 0.0f;
+                        
+                        [self.view layoutIfNeeded];
+                        [_dynamicView layoutIfNeeded];
+                        [lineDynamicView layoutIfNeeded];
+                        [vesselDynamicView layoutIfNeeded];
+                    } completion:^(BOOL finished) {
+                        [_dynamicView setHidden:YES];
+                        if (recognizer) {
+                            [self removeGestureRecognizer];
+                        }
+                        [_tableView setScrollEnabled:YES];
+                    }];
+                } else if (rec.state == UIGestureRecognizerStateBegan || rec.state == UIGestureRecognizerStateChanged) {
+                    //when pan, what we need do:
+                    //1.change dynamicView orgin
+                    //2.add a blur view
+                    //3.when translation.y == dynamic.height, blurView dismiss
+                }
+
+//                
+//            }
+        }
+    }
+}
+
+
 #pragma mark - view operation
 - (void)increaseDynamicViewHeight {
     [_dynamicView mas_updateConstraints:^(MASConstraintMaker *make) {
@@ -190,41 +272,66 @@
 #pragma mark - button event
 - (void)vesselBtnClick:(UIButton *)btn {
     NSLog(@"vesselBtnClick");
+    _dynamicView.hidden = NO;
+    lineDynamicView.hidden = YES;
+    vesselDynamicView.hidden = NO;
     [UIView animateWithDuration:0.5f animations:^{
         [self increaseDynamicViewHeight];
         lineDynamicView.alpha = 0.0f;
         vesselDynamicView.alpha = 1.0f;
+        _dynamicView.alpha = 1.0f;
         [vesselBtn setBackgroundColor:[UIColor colorWithRed:102.0/255.0 green:161.0/255.0 blue:115.0/255.0 alpha:1.0f]];
         [vesselBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [lineBtn setBackgroundColor:[UIColor whiteColor]];
         [lineBtn setTitleColor:[UIColor colorWithRed:102.0/255.0 green:161.0/255.0 blue:115.0/255.0 alpha:1.0f] forState:UIControlStateNormal];
         [self.view layoutIfNeeded];
         [_dynamicView layoutIfNeeded];
+        [lineDynamicView layoutIfNeeded];
+        [vesselDynamicView layoutIfNeeded];
     } completion:^(BOOL finished) {
-        lineDynamicView.hidden = YES;
-        vesselDynamicView.hidden = NO;
+//        if (!recognizer) {
+            [self initGestureRecognizer];
+            [_tableView setScrollEnabled:NO];
+//        }
     }];
 }
 
 - (void)lineBtnClick:(UIButton *)btn {
     NSLog(@"lineBtnClick");
+    _dynamicView.hidden = NO;
+    lineDynamicView.hidden = NO;
+    vesselDynamicView.hidden = YES;
     [UIView animateWithDuration:0.5f animations:^{
         [self increaseDynamicViewHeight];
         lineDynamicView.alpha = 1.0f;
         vesselDynamicView.alpha = 0.0f;
+        _dynamicView.alpha = 1.0f;
         [lineBtn setBackgroundColor:[UIColor colorWithRed:102.0/255.0 green:161.0/255.0 blue:115.0/255.0 alpha:1.0f]];
         [lineBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [vesselBtn setBackgroundColor:[UIColor whiteColor]];
         [vesselBtn setTitleColor:[UIColor colorWithRed:102.0/255.0 green:161.0/255.0 blue:115.0/255.0 alpha:1.0f] forState:UIControlStateNormal];
         [self.view layoutIfNeeded];
         [_dynamicView layoutIfNeeded];
+        [lineDynamicView layoutIfNeeded];
+        [vesselDynamicView layoutIfNeeded];
     } completion:^(BOOL finished) {
-        lineDynamicView.hidden = NO;
-        vesselDynamicView.hidden = YES;
+//        if (!recognizer) {
+            [self initGestureRecognizer];
+            [_tableView setScrollEnabled:NO];
+//        }
     }];
 }
 
 #pragma mark - tableview datasource delegate and datasource
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView == _tableView && _dynamicView.bounds.size.height > 0) {
+        if (!recognizer) {
+            [self initGestureRecognizer];
+        }
+        [_tableView setScrollEnabled:NO];
+    }
+}
+
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 100;
 }
@@ -239,12 +346,34 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     }
     cell.textLabel.text = @"test";
+    cell.showsReorderControl = YES;
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
+//- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+//    return YES;
+//}
+//
+//- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    [tableView setEditing:YES animated:YES];
+//    return UITableViewCellEditingStyleDelete;
+//}
+//
+//- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+//    [tableView setEditing:NO animated:YES];
+//}
+//
+//- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+//    return YES;
+//}
+//
+//- (BOOL)tableView:(UITableView *)tableView shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath *)indexPath {
+//    return NO;
+//}
 
 /*
 #pragma mark - Navigation
